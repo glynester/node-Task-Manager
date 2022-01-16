bc28310d7f2c3ab03d857300d507b3713ad3a2b8
Gee <glynester@hotmail.com>
2022-01-16 21:45:34 +0000

    2 endpoints added for task - find & findById

--- src/index.js
+++ src/index.js
@@ -19,6 +19,27 @@ app.post('/users',(req,res)=>{
   // res.send('testing');
 })
 
+app.get('/tasks',(req, res)=>{
+  Task.find({}).then((tasks)=>{
+    res.status(200).send(tasks);
+  }).catch((error)=>{
+    res.status(500).send();
+  })
+})
+
+app.get('/tasks/:id',(req,res)=>{
+  const _id = req.params.id;
+  Task.findById(_id).then((task)=>{
+    if (!task){
+      return res.status(404).send();
+    }
+    res.status(200).send(task);
+  }).catch((error)=>{
+    res.status(500).send();
+  })
+
+})
+
 app.get('/users',(req,res)=>{
   User.find({}).then((users)=>{
     res.status(200).send(users);
--- src/models/task.js
+++ src/models/task.js
@@ -16,38 +16,4 @@ const Task = mongoose.model('Task',{
 module.exports=Task;
 
 
-// const task=new Task({
-//   description: "         Start to do list.    ",
-//   // completed: false,
-// })
-// task.save().then(()=>{
-//   console.log(task);
-// }).catch((error)=>{
-//   console.log(error);
-// })
 
-
-// db.collection('tasks').updateMany({
-  //   completed:false,
-  // },{
-  //   $set:{
-  //     completed: true,
-  //   }
-  // }).then((result)=>{
-  //   console.log(result);
-  // }).catch((error)=>{
-  //   console.log(error);
-  // })
-  
-  
-
-  // db.collection('tasks').findOne({_id:ObjectId("61e081f9d317d63e11b0b8a9")},(error,task)=>{
-  //   console.log(task);
-  // })
-
-  // db.collection('tasks').find({completed:false}).toArray((error, tasks)=>{
-  //   if (error){
-  //     return console.log('Can\'t find tasks');
-  //   }
-  //   console.log(tasks);
-  // })
