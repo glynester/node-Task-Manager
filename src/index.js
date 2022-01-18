0973ea6d8fb6497abc2058ed01abad7537665b4e
Gee <glynester@hotmail.com>
2022-01-18 10:30:39 +0000

    New routes for delete user and delete task

--- src/index.js
+++ src/index.js
@@ -89,6 +89,18 @@ app.patch('/tasks/:id',async (req, res)=>{
   }
 })
 
+app.delete('/tasks/:id',async (req,res)=>{
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task){
+      return res.status(404).send({error: "Task not found"})
+    }
+    return res.status(200).send(task)
+  } catch(e){
+    return res.status(500).send(e);
+  }
+})
+
 app.post('/users',async(req,res)=>{
   const user = new User(req.body);
   try {
@@ -174,6 +186,18 @@ app.patch('/users/:id',async (req, res)=>{
     res.status(400).send(e);
   }
   
+})
+
+app.delete('/users/:id',async (req,res)=>{
+  try {
+    const user=await User.findByIdAndDelete(req.params.id);
+    if (!user){
+      return res.status(404).send({error: "No such user"})
+    }
+    res.status(200).send(user);
+  } catch(e){
+    res.status(500).send(e);
+  }
 })
 
 app.listen(port,()=>{
