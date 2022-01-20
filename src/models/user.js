f67ee25caf7058fb4d40f52d4c147c776f19a6df
Gee <glynester@hotmail.com>
2022-01-20 19:45:26 +0000

    Added 'pre' middleware to also delete tasks belonging to a user who is deleted (remove).

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');   // Validation package
 const bcrypt=require('bcryptjs');
 const jwt=require('jsonwebtoken');
+const Task=require('./task');
 // Pass model object to schema
 // const userSchema = new mongoose.Schema({*user_model_object});
 
@@ -113,6 +114,15 @@ userSchema.pre('save',async function(next){
   }
   // console.log('Just before saving - after pword hash',user);
   next();   // means we're done. Will hang if we don't call next().
+})
+
+// Middleware to delete user tasks when user is removed.
+userSchema.pre('remove', async function(next){
+  const user=this;
+  await Task.deleteMany({
+    owner: user._id,
+  })
+
 })
 
 const User = mongoose.model('User',userSchema);
--- src/routers/user.js
+++ src/routers/user.js
@@ -66,7 +66,7 @@ router.patch('/users/me', auth, async (req, res)=>{
 
 router.delete('/users/me', auth, async (req,res)=>{
   try {
-    await req.user.remove();
+    await req.user.remove();    // Middleware on User model runs just before runs to delete the user's tasks
     res.status(200).send(req.user);
   } catch(e){
     res.status(500).send(e);
