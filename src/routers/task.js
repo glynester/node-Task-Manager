a07ed7887d3e1e523506cd06062f0ed2e63bd028
Gee <glynester@hotmail.com>
2022-01-20 19:05:10 +0000

    Removed learning comments from task routes file

--- src/routers/task.js
+++ src/routers/task.js
@@ -3,14 +3,8 @@ const Task=require('../models/task');
 const auth=require('../middleware/auth');
 const router=new express.Router();
 
-// router.get('/test',(req,res)=>{
-//   res.send('Router from task!!!');
-// })
-// router.post('/tasks',async (req,res)=>{
 router.post('/tasks', auth, async (req,res)=>{
-  // const task=new Task(req.body);
   const task = new Task({
-    // spread operator used to incorporate original object into new object.
     ...req.body,
     owner: req.user._id,
   })
@@ -22,23 +16,9 @@ router.post('/tasks', auth, async (req,res)=>{
   }
 })
 
-// app.post('/tasks',(req,res)=>{
-//   const task=new Task(req.body);
-//   task.save().then(()=>{
-//     res.status(201).send(task); // more appropriate status code - "201 Created"
-//   }).catch((error)=>{
-//     res.status(400).send(error);
-//   })
-// })
-
 router.get('/tasks',auth, async(req, res)=>{
   try {
-    // 2 ways to do this. First option:
-    // const tasks=await Task.find({
-    //   owner: req.user._id,
-    // });
-    // Need to send(task) for 1st option.
-    // 2nd option:
+    // const tasks=await Task.find({ owner: req.user._id, });  // Another way to do this!
     await req.user.populate('tasks');
     res.status(200).send(req.user.tasks);
   } catch(e){
@@ -46,18 +26,9 @@ router.get('/tasks',auth, async(req, res)=>{
   }
 })
 
-// app.get('/tasks',(req, res)=>{
-//   Task.find({}).then((tasks)=>{
-//     res.status(200).send(tasks);
-//   }).catch((error)=>{
-//     res.status(500).send();
-//   })
-// })
-
 router.get('/tasks/:id', auth, async(req,res)=>{
   const _id = req.params.id;
   try {
-    // const task=await Task.findById(_id);
     const task = await Task.findOne({
       _id,
       owner: req.user._id,
@@ -71,32 +42,13 @@ router.get('/tasks/:id', auth, async(req,res)=>{
   }
 })
 
-// app.get('/tasks/:id',(req,res)=>{
-//   const _id = req.params.id;
-//   Task.findById(_id).then((task)=>{
-//     if (!task){
-//       return res.status(404).send();
-//     }
-//     res.status(200).send(task);
-//   }).catch((e)=>{
-//     res.status(500).send(e);
-//   })
-// })
-
 router.patch('/tasks/:id', auth, async (req, res)=>{
   const updates=Object.keys(req.body);
-  const allowedUpdates=['description','completed'];  // Without this mongoose doesn't throw an error if you try to update something that doesn't exist like {'religion':'agnostic'}
+  const allowedUpdates=['description','completed'];
   if (!updates.every(v=>allowedUpdates.includes(v))){
     return res.status(400).send({error: "Property to update doesn't exist"})
   }
   try {
-    // Need to split out the "save" action so it can be intercepted by the middleware to do a "pre" save update of the plain text password to a hashed password.
-    // const task=await Task.findByIdAndUpdate(req.params.id,req.body,{
-    //   new:true,
-    //   runValidators:true,
-    // })
-    // The lines below (up to 'await task.save()') replace commented code above. Need to split out findByIdAndUpdate so that the middleware can be used, as it runs "pre" to 'save' running. 
-    // const task=await Task.findById(req.params.id);
     const task = await Task.findOne({
       _id: req.params.id, 
       owner: req.user._id,
@@ -112,10 +64,8 @@ router.patch('/tasks/:id', auth, async (req, res)=>{
   }
 })
 
-// router.delete('/tasks/:id',async (req,res)=>{
 router.delete('/tasks/:id', auth, async (req,res)=>{
   try {
-    // const task = await Task.findByIdAndDelete(req.params.id);
     const task = await Task.findOneAndDelete({
       _id: req.params.id,
       owner: req.user._id,
