317ea6148e276f48b4c69c1f80eb343db457f47d
Gee <glynester@hotmail.com>
2022-01-22 12:32:18 +0000

    Set up @sendgrid/mail for emails and added auto emails to create and delete user routes.

---
+++ src/emails/account.js
@@ -0,0 +1,49 @@
+const sgMail = require('@sendgrid/mail');
+
+const sendgridAPIKey = 'XXXXXXXXXXXXXXXXXX';
+
+sgMail.setApiKey(sendgridAPIKey);
+
+const sendWelcomeEmail = (email, name)=>{
+  // This is aynchronous - send method returns a promise.
+  sgMail.send({
+    to: email,
+    from: 'glynester@hotmail.com',
+    subject: 'Welcome to the Task-Manager App ' + name,
+    text: `Welcome to Task-Manager ${name}
+    We hope you enjoy your stay.
+    Thanks 
+    The Task-Manager Team`
+  })
+}
+
+const sendCancellationEmail = (email, name)=>{
+  sgMail.send({
+    to: email,
+    from: 'glynester@hotmail.com',
+    subject: 'Sorry to see you leave ' + name,
+    text: `
+    We're sorry you're leaving Task-Manager ${name}
+    We hope you enjoyed your stay. 
+    If there's anything we can do to improve the app, please let us know.
+    Thanks 
+    The Task-Manager Team`
+  })
+}
+
+// sgMail.send({
+//   to: 'glennbem@gmail.com',
+//   from: 'glynester@hotmail.com',
+//   subject: 'E-mail sent using Twilio Sendgrid and nodejs',
+//   text: `The Sun is the star at the centre of the Solar System. It is a 
+//   nearly perfect ball of hot plasma, heated to incandescence by nuclear 
+//   fusion reactions in its core, radiating the energy mainly as visible 
+//   light, ultraviolet light, and infrared radiation.`,
+//   // html:      // Another property
+// })
+
+
+module.exports = {
+  sendWelcomeEmail,
+  sendCancellationEmail,
+}
--- src/routers/user.js
+++ src/routers/user.js
@@ -2,13 +2,16 @@ const express=require('express');
 const multer=require('multer');
 const sharp=require('sharp');
 const User=require('../models/user');
-const auth=require('../middleware/auth')
+const auth=require('../middleware/auth');
+const {sendWelcomeEmail, sendCancellationEmail}=require('../emails/account');
 const router=new express.Router();
+
 
 router.post('/users',async(req,res)=>{
   const user = new User(req.body);
   try {
     await user.save();
+    sendWelcomeEmail(user.email, user.name);    // Although this is asynchronous there is no need to 'await' it.
     const token = await user.generateAuthToken();
     res.status(201).send({user,token,loginStatus:"YOU ARE (in theory) LOGGED IN!!!"});
   } catch(e){
@@ -69,6 +72,7 @@ router.patch('/users/me', auth, async (req, res)=>{
 router.delete('/users/me', auth, async (req,res)=>{
   try {
     await req.user.remove();    // Middleware on User model runs just before runs to delete the user's tasks
+    sendCancellationEmail(req.user.email, req.user.name);
     res.status(200).send(req.user);
   } catch(e){
     res.status(500).send(e);
